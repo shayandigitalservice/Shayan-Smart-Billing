cb507906fa75dbe1673ef69cecd32000c0680143
Shayan Smart Tools <264066508+shayandigitalservice@users.noreply.github.com>
2026-08-01 15:12:57 +0530

    Update service worker to cache additional files
    Added files to cache for offline support.

--- sw.js
+++ sw.js
@@ -1,9 +1,18 @@
 const CACHE_NAME = 'shayan-pos-v1';
 
+// Files ko offline support ke liye cache me add karna
+const FILES_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(['./']);
+      return cache.addAll(FILES_TO_CACHE);
     })
   );
 });
